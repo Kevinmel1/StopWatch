28c0fe1210e42f53202f9e2188e31e2ef75e45bc
Sinbad1 <fatyr13@gmail.com>
2020-12-13 14:42:12 +0100

    Initial commit

---
+++ StopWatch/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace StopWatch
+{
+    class Program
+    {
+        static void Main()
+        {
+            bool isRunning = true;
+            var stopWatch = new StopWatch();
+
+            while (isRunning)
+            {
+                Console.WriteLine("Tap + Enter: 'y' to start, 'n' to stop, or 'q' to quit.");
+                try
+                {
+                    switch (Console.ReadLine().ToLower())
+                    {
+                        case "y":
+                            stopWatch.Start();
+                            Console.WriteLine($"Stopwatch started at: {DateTime.Now:H:mm:ss}");
+                            break;
+
+                        case "n":
+                            stopWatch.Stop();
+                            Console.WriteLine($"Stopwatch stopped at: {DateTime.Now:H:mm:ss} Run time: {stopWatch.Duration}");
+                            break;
+
+                        case "q":
+                            return;
+                    }
+                } catch (InvalidOperationException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+    }
+}
---
+++ StopWatch/StopWatch.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace StopWatch
+{
+    public class StopWatch
+    {
+        private DateTime _startTimer;
+        private DateTime _stopTimer;
+        private TimeSpan _duration;
+        private bool _isStarted = false;
+
+        public TimeSpan Duration
+        {
+            get { return _duration; }
+            set { _duration = value; }
+        }
+
+        public void Start()
+        {
+            if (!_isStarted)
+            {
+                _startTimer = DateTime.UtcNow;
+                Duration = _startTimer.TimeOfDay;
+                _isStarted = true;
+            } else
+            {
+                throw new InvalidOperationException("You must stop before starting the timer again.\n");
+            }
+        }
+
+        public TimeSpan Stop()
+        {
+            if (_isStarted)
+            {
+                _stopTimer = DateTime.UtcNow;
+                _isStarted = false;
+            } else
+            {
+                throw new InvalidOperationException("Timer isn't started.\n");
+            }
+            return Duration = _stopTimer.Subtract(_startTimer);
+        }
+    }
+}
